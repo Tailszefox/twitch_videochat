4d9484aac708e4c880f8d286a2c7d5b7393eb6f3
Tailszefox <tails@tailszefox.net>
2017-08-03 01:59:25 +0200

    Ignore room commands

--- make_frames.py
+++ make_frames.py
@@ -37,6 +37,10 @@ messageNo = 0
 for m in tqdm(chat, unit = "messages"):
     messageNo += 1
 
+    # Ignore room commands
+    if m["attributes"]["command"] == "ROOMSTATE":
+        continue
+
     newMessage = ChatMessage(m)
     messages.append(newMessage)
 
